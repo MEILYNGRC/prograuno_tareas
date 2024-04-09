adbed01dcfa6894dbb7a3b724515016f81200c0c
Meilyn García <133615693+MEILYNGRC@users.noreply.github.com>
2024-04-08 23:28:33 -0600

    Add files via upload

---
+++ encabezado e implementacion/include/Notas facultad .h
@@ -0,0 +1,16 @@
+#ifndef NOTAS FACULTAD _H
+#define NOTAS FACULTAD _H
+
+
+class Notas facultad
+{
+    public:
+        Notas facultad ();
+        virtual ~Notas facultad ();
+
+    protected:
+
+    private:
+};
+
+#endif
---
+++ encabezado e implementacion/main.cpp
@@ -0,0 +1,25 @@
+#ifndef NOTAS FACULTAD_H
+#define NOTAS FACULTAD_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+//PARA MATRIZ
+#define NUMERO_ALUMNOS 5
+#define NUMERO_MATERIAS 5
+#define NUMERO_NOTAS 4
+#define MAX_CALIFICACION 100
+#define MIN_CALIFICACION 0
+#define MAXIMA_LONGITUD_CADENA 100
+
+void llamaCiclo();
+int busquedaAleatorios(int minimo, int maximo);
+void llenarMatriz(float matriz[NUMERO_ALUMNOS][NUMERO_NOTAS + 1]);//duda 1
+void imprimirMatrizLinea();
+float imprimirMatriz(float matriz[NUMERO_ALUMNOS][NUMERO_NOTAS + 1], char alumnos[NUMERO_ALUMNOS][MAXIMA_LONGITUD_CADENA], string nombreFacultad);
+
+#endif
